2746b696969afca04c5f496ddb2799c54c74e12f
nbzongzong <73403523+nbzongzong@users.noreply.github.com>
2020-12-21 15:12:05 +0800

    Update jd_jdfactory.js

--- jd_jdfactory.js
+++ jd_jdfactory.js
@@ -52,7 +52,7 @@ if ($.isNode()) {
 }
 let wantProduct = ``;//心仪商品名称
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-const inviteCodes = [``];
+const inviteCodes = [`P04z54XCjVWnYaS5mtaQj-mijYVyw8','P04z54XCjVWnYaS5jQNCmXw3HVPnHcN_b8','P04z54XCjVWnYaS5m9cZ2Wt3X1OkD1bmasb_7E','P04z54XCjVWnYaS5m9cZ2Xwjn4fwWkbfJuvhRw`];
 !(async () => {
   await requireConfig();
   if (!cookiesArr[0]) {
@@ -648,10 +648,10 @@ function shareCodesFormat() {
       const tempIndex = $.index > inviteCodes.length ? (inviteCodes.length - 1) : ($.index - 1);
       $.newShareCodes = inviteCodes[tempIndex].split('@');
     }
-    const readShareCodeRes = await readShareCode();
-    if (readShareCodeRes && readShareCodeRes.code === 200) {
-      $.newShareCodes = [...new Set([...$.newShareCodes, ...(readShareCodeRes.data || [])])];
-    }
+ //   const readShareCodeRes = await readShareCode();
+  //  if (readShareCodeRes && readShareCodeRes.code === 200) {
+ //     $.newShareCodes = [...new Set([...$.newShareCodes, ...(readShareCodeRes.data || [])])];
+  //  }
     console.log(`第${$.index}个京东账号将要助力的好友${JSON.stringify($.newShareCodes)}`)
     resolve();
   })

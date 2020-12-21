7b9e0b35aa1fe025f8acdaa232ce084c0fe20673
nbzongzong <73403523+nbzongzong@users.noreply.github.com>
2020-12-21 14:39:09 +0800

    Update jd_dreamFactory.js

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -34,7 +34,7 @@ const randomCount = $.isNode() ? 20 : 5;
 const tuanActiveId = `jfkcidGQavswLOBcAWljrw==`;
 const jxOpenUrl = `openjd://virtual?params=%7B%20%22category%22:%20%22jump%22,%20%22des%22:%20%22m%22,%20%22url%22:%20%22https://wqsd.jd.com/pingou/dream_factory/index.html%22%20%7D`;
 let cookiesArr = [], cookie = '', message = '';
-const inviteCodes = [''];
+const inviteCodes = ['NeuVHXg0vw8-QRJS6fmL7A==','_BDKVoB5GwiWpDGS-XmjGw==','HUJ2O_0p_vfKmdtrd1PH6A==','ywJR3XfRtJKSZEO3L-AGQw=='];
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {

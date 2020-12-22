88feea276da12b809cf3c0b66cb0ab5eb3a3b5eb
nbzongzong <73403523+nbzongzong@users.noreply.github.com>
2020-12-22 23:13:01 +0800

    修复东东小窝自己账号相互助力功能

--- jd_small_home.js
+++ jd_small_home.js
@@ -94,9 +94,9 @@ const JD_API_HOST = 'https://lkyl.dianpusoft.cn/api';
       cookie = cookiesArr[i];
       $.UserName = decodeURIComponent(cookie.match(/pt_pin=(.+?);/) && cookie.match(/pt_pin=(.+?);/)[1])
       if ($.newShareCodes.length > 1) {
-        let code = $.newShareCodes[(i + 1) % $.newShareCodes.length]
+        let code = $.newShareCodes[(i + 1) % $.newShareCodes.length]['code']
         console.log(`\n${$.UserName}去给自己的下一账号${decodeURIComponent(cookiesArr[(i + 1) % $.newShareCodes.length].match(/pt_pin=(.+?);/) && cookiesArr[(i + 1) % $.newShareCodes.length].match(/pt_pin=(.+?);/)[1])}助力\n`)
-        await createAssistUser(code, $.createAssistUserID || "1318106976846299138");
+        await createAssistUser(code, $.createAssistUserID);
       }
       console.log(`\n去帮助作者:lxk0301\n`)
 	  console.log(`\n不去帮助作者:lxk0301\n`)
@@ -153,7 +153,7 @@ async function helpFriends() {
   if (!$.inviteCodes) await updateInviteCodeCDN('https://gitee.com/lxk0301/updateTeam/raw/master/jd_updateSmallHomeInviteCode.json');
   for (let item of $.inviteCodes.inviteCode) {
     if (!item) continue
-    await createAssistUser(item, $.createAssistUserID || "1318106976846299138");
+    await createAssistUser(item, $.createAssistUserID);
   }
 }
 async function doAllTask() {
@@ -165,7 +165,6 @@ async function doAllTask() {
   for (let item of $.taskList) {
     if (item.ssjjTaskInfo.type === 1) {
       //邀请好友助力自己
-      // await createAssistUser('1330186694770339842', item.ssjjTaskInfo.id)
       $.createAssistUserID = item.ssjjTaskInfo.id;
       console.log(`\n\n助力您的好友:${item.doneNum}人`)
     }
@@ -504,7 +503,7 @@ function createInviteUser() {
                 if (data.body.id) {
                   console.log(`\n您的${$.name}shareCode(每天都是变化的):【${data.body.id}】\n`);
                   $.shareCode = data.body.id;
-                  $.newShareCodes.push(data.body.id);
+                   $.newShareCodes.push({ 'code': data.body.id, 'token': $.token });
                 }
               }
             }
@@ -520,6 +519,7 @@ function createInviteUser() {
 }
 
 function createAssistUser(inviteId, taskId) {
+  console.log(`${inviteId}, ${taskId}`, `${cookie}`);
   return new Promise(resolve => {
     $.get(taskUrl(`/ssjj-task-record/createAssistUser/${inviteId}/${taskId}`), (err, resp, data) => {
       try {

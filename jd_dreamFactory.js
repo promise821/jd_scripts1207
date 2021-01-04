084e212425fd26aa5b7cbf8e82bec691858e8277
wang_j2 <jun.wang@preh.cn>
2021-01-04 18:02:57 +0800

    Update jd_dreamFactory.js

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -1036,7 +1036,7 @@ async function joinLeaderTuan() {
   // }
 }
 function JoinTuan(tuanId) {
-	console.log(`准备加入tuanId ${tuanId}`);
+	console.log(`尝试参团 ${tuanId}`);
   return new Promise((resolve) => {
     const options = {
       'url': `https://m.jingxi.com/dreamfactory/tuan/JoinTuan?activeId=${escape(tuanActiveId)}&tuanId=${escape(tuanId)}&_time=${Date.now()}&_=${Date.now()}&sceneval=2&g_login_type=1`,

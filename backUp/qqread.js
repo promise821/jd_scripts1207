f214293e5d62103520b0c35ae8c4070ac76b4a8c
nbzongzong <73403523+nbzongzong@users.noreply.github.com>
2021-01-05 23:39:11 +0800

    20210105

--- backUp/qqread.js
+++ backUp/qqread.js
@@ -62,7 +62,9 @@ const jsname = '企鹅读书'
 const $ = Env(jsname)
 let task = '', config, ssr2 = '', wktime, day = 0;
 console.log(`\n========= 脚本执行时间(TM)：${new Date(new Date().getTime() + 0 * 60 * 60 * 1000).toLocaleString('zh', {hour12: false})} =========\n`)
-const notify = require('../sendNotify');
+
+const notify = $.isNode() ? require('../sendNotify') : '';
+
 const logs = 1;   //0为关闭日志，1为开启
 
 const TIME = 30//单次时长上传限制，默认5分钟
@@ -235,7 +237,7 @@ async function QQ_READ() {
 function showmsg() {
   return new Promise(async resolve => {
     let nowTimes = new Date(new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000);
-    if (nowTimes.getHours() === 22 && (nowTimes.getMinutes() >= 0 && nowTimes.getMinutes() <= 20)) {
+    if (nowTimes.getHours() === 22 && (nowTimes.getMinutes() >= 0 && nowTimes.getMinutes() <= 15)) {
       await notify.sendNotify(jsname, tz);
     }
     $.msg(jsname, "", tz);

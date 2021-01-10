215899ba544a58ee89f330f778280247627fb85e
nbzongzong <73403523+nbzongzong@users.noreply.github.com>
2021-01-11 00:03:52 +0800

    修复获取京喜农场active时的报错

--- jd_jxnc.js
+++ jd_jxnc.js
@@ -36,7 +36,7 @@ let currentCookie = ''; // 当前用户 cookie
 let tokenNull = {'farm_jstoken': '', 'phoneid': '', 'timestamp': ''}; // 内置一份空的 token
 let tokenArr = []; // 用户 token 数组
 let currentToken = {}; // 当前用户 token
-const shareCode = 'bad24e6c6ce2ff7de8e35c4bd141769f@00c93d09156384b7a932617aed24602f@fe3682c5e0c41eda7917cb335c1bde00@fe8b15a9d3404a44847ec1e0779f0247'; // 内置助力码 '22bd6fbbabbaa770a45ab2607e7a1e8a@197c6094e965fdf3d33621b47719e0b1'
+const shareCode = 'bad24e6c6ce2ff7de8e35c4bd141769f@00c93d09156384b7a932617aed24602f@fe3682c5e0c41eda7917cb335c1bde00@fe8b15a9d3404a44847ec1e0779f0247'; // 内置助力码 
 let jxncShareCodeArr = []; // 用户 助力码 数组
 let currentShareCode = []; // 当前用户 要助力的助力码
 const openUrl = `openjd://virtual?params=${encodeURIComponent('{ "category": "jump", "des": "m", "url": "https://wqsh.jd.com/sns/201912/12/jxnc/detail.html?ptag=7155.9.32&smp=b47f4790d7b2a024e75279f55f6249b9&active=jdnc_1_chelizi1205_2"}',)}`; // 打开京喜农场
@@ -239,7 +239,7 @@ async function jdJXNC() {
         await $.wait(500);
         await createAssistUser();
     }
-   await showMsg()
+    await showMsg()
 }
 
 // 获取任务列表与用户信息

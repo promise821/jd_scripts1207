c833c2a7660f0b899d34358636f07445ce650cdb
promise821 <61649347+promise821@users.noreply.github.com>
2021-01-12 01:28:53 -0600

    Add files via upload

--- jd_get_share_code.js
+++ jd_get_share_code.js
@@ -22,7 +22,7 @@ cron "20 13 * * 6" script-path=https://raw.githubusercontent.com/lxk0301/jd_scri
  */
 const $ = new Env("获取互助码");
 const JD_API_HOST = "https://api.m.jd.com/client.action";
-let ddgcArr = [],jxgcArr = [],jxncArr = [],jdzzArr = [],jdmcArr = [],zdddArr = [],jdncArr = [],crjoyArr = []
+let ddgcArr = [],jxgcArr = [],jxncArr = [],jdzzArr = [],jdmcArr = [],zdddArr = [],jdncArr = [],crjoyArr = [];
 let cookiesArr = [], cookie = '', message;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 !function (n) {

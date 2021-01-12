06807aeb035a470964fd80d6ec6ddeddbdca3af0
promise821 <61649347+promise821@users.noreply.github.com>
2021-01-12 02:01:46 -0600

    Add files via upload

--- jd_get_share_code.js
+++ jd_get_share_code.js
@@ -178,8 +178,8 @@ if ($.isNode()) {
   }
 console.log(`整理助力码输出`);
   console.log(`============`)
-console.log(`\n东东工厂：`ddgcArr, `\n\n京喜工厂：`jxgcArr, `\n\n京东赚赚：`\n\njdzzArr, `\n\n京喜农场：`jxncArr, `\n\n京东萌宠：`jdmcArr, `\n\n种豆得豆：`zdddArr, `\n京东农场：`jdncArr, `\n疯狂joy：`crjoyArr);
- await notify.sendNotify(`助力码推送`, `\n东东工厂：`ddgcArr, `\n\n京喜工厂：`jxgcArr, `\n\n京东赚赚：`\n\njdzzArr, `\n\n京喜农场：`jxncArr, `\n\n京东萌宠：`jdmcArr, `\n\n种豆得豆：`zdddArr, `\n京东农场：`jdncArr, `\n疯狂joy：`crjoyArr);
+console.log(`\r\n东东工厂：`ddgcArr, \r\n\n京喜工厂：`jxgcArr, `\r\n\n京东赚赚：`\n\njdzzArr, `\r\n\n京喜农场：`jxncArr, `\r\n\n京东萌宠：`jdmcArr, `\r\n\n种豆得豆：`zdddArr, `\n京东农场：`jdncArr, `\r\n疯狂joy：`crjoyArr);
+ await notify.sendNotify(`助力码推送`, `\r\n东东工厂：`ddgcArr, \r\n\n京喜工厂：`jxgcArr, `\r\n\n京东赚赚：`\n\njdzzArr, `\r\n\n京喜农场：`jxncArr, `\r\n\n京东萌宠：`jdmcArr, `\r\n\n种豆得豆：`zdddArr, `\n京东农场：`jdncArr, `\r\n疯狂joy：`crjoyArr);
 console.log(`============`)
 
 })()

c1a72475d895161612e706c2fbac6f5d7a4ebedc
wang_j2 <jun.wang@preh.cn>
2021-01-19 14:41:48 +0800

    Update getJDCookie.js

--- getJDCookie.js
+++ getJDCookie.js
@@ -7,12 +7,15 @@
  * Modify from FanchangWang https://github.com/FanchangWang
  */
 const $ = new Env('扫码获取京东cookie');
+const notify = $.isNode() ? require('./sendNotify') : '';
 const qrcode = require('qrcode-terminal');
-let s_token, cookies, guid, lsid, lstoken, okl_token, token
+let s_token, cookies, guid, lsid, lstoken, okl_token, token;
+let message = '';
 !(async () => {
   await loginEntrance();
   await generateQrcode();
   await getCookie();
+  await notify.sendNotify(`${$.UserName} Cookie获取成功` ,message);
 })()
     .catch((e) => {
       $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '')
@@ -145,7 +148,9 @@ function formatCookie(headers) {
 
     $.UserName = decodeURIComponent(cookie1.match(/pt_pin=(.+?);/) && cookie1.match(/pt_pin=(.+?);/)[1])
     $.log(`京东用户：${$.UserName} Cookie获取成功(有效期：${headers['strict-transport-security'].substring("max-age=7776000".indexOf('=') + 1, "max-age=7776000".length)}秒)，cookie如下：`);
-    $.log(`\n${cookie1}\n`);
+    //$.log(`\n${cookie1}\n`);
+	$.log(`\ncookie已通过通知发送，请查收\n`);
+	message += `cookie如下：\n${cookie1}\n`;
     resolve()
   })
 }

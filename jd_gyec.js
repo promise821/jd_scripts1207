29c4aba1f2a1230f7b7595d3846554c92b4b7eaf
promise821 <niaolin821@126.com>
2021-01-20 09:04:26 +0800

    Update jd_gyec.js

--- jd_gyec.js
+++ jd_gyec.js
@@ -31,8 +31,8 @@ const $ = new Env('工业品爱消除');
 const notify = $.isNode() ? require('./sendNotify') : '';
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let inviteCodes = [
-  '840266@2583822@2585219@2586018@1556311@2583822@2585256@2586023@2728968',
-  '840266@2583822@2585219@2586018@1556311@2583822@2585256@2586023@2728968',
+  '',
+  '',
 ]
 const ACT_ID = 'A_112790_R_4_D_20201209'
 let exchangeName = $.isNode() ? (process.env.EXCHANGE_GYEC ? process.env.EXCHANGE_GYEC : '1888京豆') : ($.getdata('JDGYEC') ? $.getdata('JDGYEC') : '1888京豆')
@@ -45,7 +45,7 @@ if ($.isNode()) {
   })
   if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => {
   };
-  if(JSON.stringify(process.env).indexOf('GITHUB')>-1) process.exit(0)
+ 
 } else {
   let cookiesData = $.getdata('CookiesJD') || "[]";
   cookiesData = jsonParse(cookiesData);

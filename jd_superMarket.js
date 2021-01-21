eccb290f72470001401f80406b01656f6b79093d
wang_j2 <jun.wang@preh.cn>
2021-01-21 17:56:41 +0800

    update

--- jd_superMarket.js
+++ jd_superMarket.js
@@ -10,15 +10,17 @@ Some Functions Modified From https://github.com/Zero-S1/JD_tools/blob/master/JD_
 支持京东双账号
 京小超兑换奖品请使用此脚本 https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_blueCoin.js
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
-// QuantumultX
+=================QuantumultX==============
 [task_local]
 #东东超市
 11 1-23/5 * * * https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_superMarket.js, tag=东东超市, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jxc.png, enabled=true
-// Loon
+===========Loon===============
 [Script]
 cron "11 1-23/5 * * *" script-path=https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_superMarket.js,tag=东东超市
-// Surge
-东东超市 = type=cron,cronexp="11 1-23/5 * * *",wake-system=1,timeout=320,script-path=https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_superMarket.js
+=======Surge===========
+东东超市 = type=cron,cronexp="11 1-23/5 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_superMarket.js
+==============小火箭=============
+东东超市 = type=cron,script-path=https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_superMarket.js, cronexpr="11 1-23/5 * * *", timeout=3600, enable=true
  */
 const $ = new Env('东东超市');
 //Node.js用户请在jdCookie.js处填写京东ck;
@@ -378,6 +380,7 @@ async function businessCircleActivity() {
             const { pkTeamPrizeInfoVO } = receivedPkTeamPrize.data.result;
             message += `【商圈PK奖励】${pkTeamPrizeInfoVO.blueCoin}蓝币领取成功\n`;
             if ($.isNode()) {
+              //屏蔽通知
               //await notify.sendNotify(`${$.name} - 账号${$.index} - ${$.nickName}`, `【京东账号${$.index}】 ${$.nickName}\n【商圈队伍】PK获胜\n【奖励】${pkTeamPrizeInfoVO.blueCoin}蓝币领取成功`) //PK结果不通知
             }
           } else if (receivedPkTeamPrize.data.result.pkResult === 2) {

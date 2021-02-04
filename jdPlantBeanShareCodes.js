a89f4bf66224c2dbb9cd29309422c9029e319e32
wang_j2 <jun.wang@preh.cn>
2021-02-04 17:33:26 +0800

    去除种豆内置助力

--- jdPlantBeanShareCodes.js
+++ jdPlantBeanShareCodes.js
@@ -8,8 +8,9 @@
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let PlantBeanShareCodes = [
-  'tj7v4vkqdyycfgggqjaub4rnxi@24mrpgihs6oezojf63n6a5l55e',//账号一的好友shareCode,不同好友中间用@符号隔开
-  'tj7v4vkqdyycfgggqjaub4rnxi@24mrpgihs6oezojf63n6a5l55e',//账号二的好友shareCode，不同好友中间用@符号隔开
+''
+  //'tj7v4vkqdyycfgggqjaub4rnxi@24mrpgihs6oezojf63n6a5l55e',//账号一的好友shareCode,不同好友中间用@符号隔开
+  //'tj7v4vkqdyycfgggqjaub4rnxi@24mrpgihs6oezojf63n6a5l55e',//账号二的好友shareCode，不同好友中间用@符号隔开
 ]
 // 判断github action里面是否有种豆得豆互助码
 if (process.env.PLANT_BEAN_SHARECODES) {

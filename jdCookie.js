155b8b889fb0d7e164b9d4e3b337005a1ea3f2ce
nbzongzong <73403523+nbzongzong@users.noreply.github.com>
2021-03-24 22:02:16 +0800

    update

--- jdCookie.js
+++ jdCookie.js
@@ -2,12 +2,11 @@
 此文件为Node.js专用。其他用户请忽略
  */
 //此处填写京东账号cookie。
-//注：github action用户cookie填写到Settings-Secrets里面，新增JD_COOKIE，多个账号的cookie使用`&`隔开或者换行
 let CookieJDs = [
   '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
   '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
 ]
-// 判断github action里面是否有京东ck
+// 判断环境变量里面是否有京东ck
 if (process.env.JD_COOKIE) {
   if (process.env.JD_COOKIE.indexOf('&') > -1) {
     console.log(`您的cookie选择的是用&隔开\n`)
@@ -20,9 +19,11 @@ if (process.env.JD_COOKIE) {
   }
 }
 CookieJDs = [...new Set(CookieJDs.filter(item => !!item))]
-console.log(`\n====================共有${CookieJDs.length}个京东账号Cookie=========\n`);
+console.log(`\n====================共${CookieJDs.length}个京东账号Cookie=========\n`);
 console.log(`==================脚本执行- 北京时间(UTC+8)：${new Date(new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000).toLocaleString()}=====================\n`)
+if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => {};
 for (let i = 0; i < CookieJDs.length; i++) {
+  if (!CookieJDs[i].match(/pt_pin=(.+?);/) || !CookieJDs[i].match(/pt_key=(.+?);/)) console.log(`\n提示:京东cookie 【${CookieJDs[i]}】填写不规范,可能会影响部分脚本正常使用。正确格式为: pt_key=xxx;pt_pin=xxx;（分号;不可少）\n`);
   const index = (i + 1 === 1) ? '' : (i + 1);
   exports['CookieJD' + index] = CookieJDs[i].trim();
 }

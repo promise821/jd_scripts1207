0679ca846be551c9e6ae2c54b552dd1402fa35d3
nbzongzong <73403523+nbzongzong@users.noreply.github.com>
2021-05-28 21:07:23 +0800

    Update jd_cash.js

--- jd_cash.js
+++ jd_cash.js
@@ -49,8 +49,8 @@ let allMessage = '';
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
     return;
   }
-  //await requireConfig()
-  //await getAuthorShareCode();
+  await requireConfig()
+  await getAuthorShareCode();
   //await getAuthorShareCode2();
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
